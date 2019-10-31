c1d63e2e9cd57bfe9b672a5d626e8fc7214b1e95
admin <admin@qq.com>
2019-10-31 09:38:06 +0800

    commit

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -1,6 +1,5 @@
 package cn.wzvtc.soft;
-
-import org.omg.CORBA.ORB;
+ import org.omg.CORBA.ORB;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,7 +16,7 @@ public class TestController {
     @RequestMapping(value="/data.json")
     public Map<String,String> bookById(){
         Map<String,String> resultMap=new HashMap<>();
-        resultMap.put("myname","刘笑锋1");
+        resultMap.put("myname","林涵冰");
         resultMap.put("mynumber","2012020045");
         return resultMap;
     }
